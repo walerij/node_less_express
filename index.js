ee7a3073bbb30bce4321b4e25bed3ba82963c226
Zhdanov Valery <becwal@yandex.ru>
2021-08-13 20:17:18 +0300

    Работа с HTML файлами

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express')
 
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const path = require('path')
 
+
+app.get("/",(req,res)=>{
+        
+        res.sendFile(path.join(__dirname,"views","index.html"))
+})
+
+app.get("/about",(req,res)=>{
+        
+    res.sendFile(path.join(__dirname,"views","about.html"))
+})
+
+
+const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
     console.log('server started on port '+PORT)
 })
