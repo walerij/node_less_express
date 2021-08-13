20f5fa46a9e92d55edcb2fb780681ad5b2699baf
Zhdanov Valery <becwal@yandex.ru>
2021-08-13 20:38:43 +0300

    Подключение handelbars и вывод представлений

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ const app = express()
 
 const path = require('path')
 
+const exphbs=require('express-handlebars')
+
+const hbs = exphbs.create({
+    defaultLayout:'main',
+    extname:'hbs'
+})
+
+app.engine('hbs',hbs.engine)
+app.set('view engine','hbs')
+app.set('views','views')
 
 app.get("/",(req,res)=>{
         
-        res.sendFile(path.join(__dirname,"views","index.html"))
+        res.render('index')
 })
 
 app.get("/about",(req,res)=>{
         
-    res.sendFile(path.join(__dirname,"views","about.html"))
+    res.render('about') 
 })
 
 
