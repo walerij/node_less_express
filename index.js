b9d4fbdb03ed97c0ca8da02d1d14e987bfff3729
Zhdanov Valery <becwal@yandex.ru>
2021-08-14 22:56:37 +0300

    Работа с формами - форма добавления курса

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.engine('hbs',hbs.engine)
 app.set('view engine','hbs')
 app.set('views','views')
 app.use(express.static('public'))
-
+app.use(express.urlencoded({extended:true}))
 app.use('/',homeRoutes)
 app.use('/add',addRoutes)
 app.use('/courses',coursesRoutes)
