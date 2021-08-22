f6593f5944c54ec58bedd1d352d25df7d71cd972
Zhdanov Valery <becwal@yandex.ru>
2021-08-22 14:05:57 +0300

    Корзина - подготовка корзины

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const path = require('path')
 const exphbs=require('express-handlebars')
 
 const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
 const addRoutes  = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 const app = express()
@@ -24,6 +25,7 @@ app.use(express.urlencoded({extended:true}))
 app.use('/',homeRoutes)
 app.use('/add',addRoutes)
 app.use('/courses',coursesRoutes)
+app.use('/card',cardRoutes)
 
 
 
---
+++ routes/card.js
@@ -0,0 +1,24 @@
+const {Router} = require("express")
+
+const Card = require('../models/card')
+const Course = require('../models/course')
+const { route } = require("./courses")
+
+const router = Router()
+
+router.post('/add',async (req,res)=>{
+    let course = await Course.getByID(req.body.id)
+    await Card.add(course)
+    res.redirect('/card')
+
+})
+
+router.get('/', async(req,res)=>{
+    let card = await Card.fetch()
+    res.render('card',{
+        title: "Корзина",
+        card
+    })
+})
+
+module.exports=router
