0915dca3458f5dc350718fcf6dd2189ec2db3647
Zhdanov Valery <becwal@yandex.ru>
2021-09-25 09:50:14 +0300

    получение данных заказов

--- routes/orders.js
+++ routes/orders.js
@@ -3,14 +3,62 @@ const Order = require('../models/order')
 const router = Router()
 
 router.get('/', async (req, res)=>{
-    res.render('orders', {
-        isOrder: true,
-        title: "Заказы"
-    })
+    try{
+        const orders = await Order.find({'user.userID':req.user._id})
+                     .populate('user.userId')
+        res.render('orders', {
+            isOrder: true,
+            title: "Заказы",
+            orders: orders.map(o=>{
+                return {
+                   ...o._doc,
+                   price: o.courses.reduce(
+                       (total,c)=>{
+                           return total+=c.count * c.course.price
+                       },0
+                   )
+                }
+            })
+        })
+    }
+    catch(e)
+    {
+        console.log(e)
+    }
+
 })
 
+
 router.post('/', async(req,res)=>{
-    res.redirect('/orders')
+
+    try{
+        const user = await req.user
+                    .populate('cart.items.courseId')
+                    .execPopulate()
+
+        const courses = user.cart.items.map(i=>({
+            count: i.count,
+            course: {...i.courseId._doc}
+        }))
+    
+        let order = new Order({
+            user: {
+                name: req.user.name,
+                userId: req.user    
+            },
+            courses: courses
+        })
+        await order.save()
+        await req.user.clearCart()
+        res.redirect('/orders')
+    }
+
+    catch(e)
+    {
+        console.log(e)
+    }
+    
+
 })
 
 module.exports = router
